3928108fe133e4424cd3effa3da2da5f5bd4f701
Ismael Valdez <seatbelts@Ismaels-MacBook-Pro.local>
2019-02-27 20:36:56 -0600

    deleted inneccesary files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-
-import { MongooseModule } from '@nestjs/mongoose';
 
 import { ItemsModule } from './items/items.module';
 
 @Module({
-  imports: [
-    MongooseModule.forRoot('mongodb://localhost/nest'),
-    ItemsModule,
-  ],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ItemsModule]
 })
 export class AppModule {}
